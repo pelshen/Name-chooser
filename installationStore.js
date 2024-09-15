7932cb6e60439071a2ec786a93450844e46b9e3b
Patrick Elshen <patrick@elshens.uk>
2024-09-15 16:13:30 +0100

    Get serverless working locally for AWS Lambda & DynamoDB

--- installationStore.js
+++ installationStore.js
@@ -1,47 +1,73 @@
-import { TableClient } from "@azure/data-tables";
+import { DynamoDBClient, PutItemCommand, GetItemCommand, DeleteItemCommand } from "@aws-sdk/client-dynamodb";
 
-const tableClient = TableClient.fromConnectionString(process.env.AZURE_TABLES_ENDPOINT, "installations");
-await tableClient.createTable();
-const buildEntity = (partitionKey, rowKey, entityData) => ({partitionKey, rowKey, entityData});
+let options = {};
 
-export const installationStore = {
-    storeInstallation: async (installation) => {
-      // Bolt will pass your handler an installation object
-      // Change the lines below so they save to your database
-      if (installation.isEnterpriseInstall && installation.enterprise !== undefined) {
-        // handle storing org-wide app installation
-        return await tableClient.upsertEntity(buildEntity('enterprise', installation.enterprise.id, JSON.stringify(installation)), "Replace");
-      }
-      if (installation.team !== undefined) {
-        // single team app installation
-        return await tableClient.upsertEntity(buildEntity('team', installation.team.id, JSON.stringify(installation)), "Replace");
-      }
-      throw new Error('Failed saving installation data to installationStore');
-    },
-    fetchInstallation: async (installQuery) => {
-      // Bolt will pass your handler an installQuery object
-      // Change the lines below so they fetch from your database
-      if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-        // handle org wide app installation lookup
-        return JSON.parse((await tableClient.getEntity('enterprise', installQuery.enterpriseId)).entityData);
-      }
-      if (installQuery.teamId !== undefined) {
-        // single team app installation lookup
-        return JSON.parse((await tableClient.getEntity('team', installQuery.teamId)).entityData);
-      }
-      throw new Error('Failed fetching installation');
-    },
-    deleteInstallation: async (installQuery) => {
-      // Bolt will pass your handler  an installQuery object
-      // Change the lines below so they delete from your database
-      if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-        // org wide app installation deletion
-        return await tableClient.deleteEntity('enterprise', installQuery.enterpriseId);
-      }
-      if (installQuery.teamId !== undefined) {
-        // single team app installation deletion
-        return await tableClient.deleteEntity('team', installQuery.teamId);
-      }
-      throw new Error('Failed to delete installation');
+// connect to local DB if running offline
+if (process.env.IS_OFFLINE) {
+  options = {
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+    credentials: {
+      accessKeyId: 'MockAccessKeyId',
+      secretAccessKey: 'MockSecretAccessKey'
     },
   };
+}
+
+const dynamoClient = new DynamoDBClient(options);
+
+const buildItem = (type, id, data) => ({ id: { S: id }, type: { S: type }, data: { S: data } });
+
+export const installationStore = {
+  storeInstallation: async (installation) => {
+    let command;
+    // Bolt will pass your handler an installation object
+    // Change the lines below so they save to your database
+    if (installation.isEnterpriseInstall && installation.enterprise !== undefined) {
+      // handle storing org-wide app installation
+      command = new PutItemCommand({ TableName: 'name-chooser_installation', Item: buildItem('enterprise', installation.enterprise.id, JSON.stringify(installation)) });
+    }
+    if (installation.team !== undefined) {
+      // single team app installation
+      command = new PutItemCommand({ TableName: 'name-chooser_installation', Item: buildItem('team', installation.team.id, JSON.stringify(installation)) });
+    }
+    if (command) {
+      return await dynamoClient.send(command);
+    }
+    throw new Error('Failed saving installation data to installationStore');
+  },
+  fetchInstallation: async (installQuery) => {
+    let command;
+    // Bolt will pass your handler an installQuery object
+    // Change the lines below so they fetch from your database
+    if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
+      // handle org wide app installation lookup
+      command = new GetItemCommand({ TableName: 'name-chooser_installation', Key: { id: { S: installQuery.enterpriseId }, type: { S: 'enterprise' } } });
+    }
+    if (installQuery.teamId !== undefined) {
+      // single team app installation lookup
+      command = new GetItemCommand({ TableName: 'name-chooser_installation', Key: { id: { S: installQuery.teamId }, type: { S: 'team' } } });
+    }
+    if (command) {
+      return JSON.parse((await dynamoClient.send(command)).Item.data.S);
+    }
+    throw new Error('Failed fetching installation');
+  },
+  deleteInstallation: async (installQuery) => {
+    let command;
+    // Bolt will pass your handler  an installQuery object
+    // Change the lines below so they delete from your database
+    if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
+      // org wide app installation deletion
+      command = new DeleteItemCommand({ TableName: 'name-chooser_installation', Key: { id: { S: installQuery.enterpriseId }, type: { S: 'enterprise' } } });
+    }
+    if (installQuery.teamId !== undefined) {
+      // single team app installation deletion
+      command = new DeleteItemCommand({ TableName: 'name-chooser_installation', Key: { id: { S: installQuery.teamId }, type: { S: 'team' } } });
+    }
+    if (command) {
+      return await dynamoClient.send(command);
+    }
+    throw new Error('Failed to delete installation');
+  },
+};
