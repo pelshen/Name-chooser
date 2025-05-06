fcf579b41cd802a8b69dd9df51e0ac0152d9c835
Patrick Elshen <patrick@elshens.uk>
2025-05-06 07:16:03 +0100

    Update

--- cms/schemaTypes/index.ts
+++ cms/schemaTypes/index.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import page from './page'
+import author from './author'
+
+export const schemaTypes = [post, page, author, category, blockContent]
